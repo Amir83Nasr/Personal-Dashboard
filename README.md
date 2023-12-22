cb7a7aa0f8ca45bc2fc1703f0e43da575d7c47e4
Amir Hossein Nasrollahi <ah12.nasrollahi@gmail.com>
2023-12-22 09:47:46 +0330

    First Push

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Personal-Dashboard
+## [Personal-Dashboard](https://amir83nasr.github.io/Personal-Dashboard) : Html5, Css3, JavaScript
+
+![All Preview](image/Preview.jpg)
---
+++ script.js
@@ -0,0 +1,47 @@
+const menuOpen = document.getElementById('menu-open')
+const menuClose = document.getElementById('menu-close')
+const sideBar = document.querySelector('.container .left-section')
+const sideBarItems = document.querySelectorAll('.container .left-section .sidebar .item')
+const bell = document.querySelector('.container .right-section .top i')
+
+menuOpen.addEventListener('click', () => {
+    sideBar.style.top = '0'
+    sideBar.style.zIndex = '1'
+
+    bell.style.zIndex = '-1'
+})
+
+menuClose.addEventListener('click', () => {
+    sideBar.style.top = '-60vh'
+    bell.style.zIndex = '0'
+})
+
+
+let activeItem = sideBarItems[0]
+
+sideBarItems.forEach(element => {
+    element.addEventListener('click', () => {
+        if (activeItem) activeItem.removeAttribute('id')
+
+        element.setAttribute('id', 'active')
+        activeItem = element
+    })
+})
+
+
+
+bell.addEventListener('click', () => {
+    if (bell.id === 'ring') bell.removeAttribute('id')
+    else bell.setAttribute('id', 'ring')
+})
+
+
+
+let icon = document.getElementById("icon")
+
+icon.addEventListener('click', () => {
+    document.body.classList.toggle("dark-theme")
+
+    if (document.body.classList.contains("dark-theme")) icon.className = "bx bxs-moon"
+    else icon.className = "bx bxs-sun"
+})
